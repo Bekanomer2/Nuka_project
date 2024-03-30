a54d6e44fe0ced4b37c4c239b1ab13402a9afbe4
Bekanomer2 <163309286+Bekanomer2@users.noreply.github.com>
2024-03-30 17:23:07 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let scrollButton = document.querySelector('.scroll-btn1');
+  
+    scrollButton.addEventListener('click', function() {
+      let targetId = this.getAttribute('data-target');
+      let targetElement = document.querySelector(targetId);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    let scrollButton = document.querySelector('.scroll-btn2');
+  
+    scrollButton.addEventListener('click', function() {
+      let targetId = this.getAttribute('data-target');
+      let targetElement = document.querySelector(targetId);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    let scrollButton = document.querySelector('.scroll-btn3');
+  
+    scrollButton.addEventListener('click', function() {
+      let targetId = this.getAttribute('data-target');
+      let targetElement = document.querySelector(targetId);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    let scrollButton = document.querySelector('.scroll-btn4');
+  
+    scrollButton.addEventListener('click', function() {
+      let targetId = this.getAttribute('data-target');
+      let targetElement = document.querySelector(targetId);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    let scrollButton = document.querySelector('.scroll-btn5');
+  
+    scrollButton.addEventListener('click', function() {
+      let targetId = this.getAttribute('data-target');
+      let targetElement = document.querySelector(targetId);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    let scrollButton = document.querySelector('.scroll-btn6');
+  
+    scrollButton.addEventListener('click', function() {
+      let targetId = this.getAttribute('data-target');
+      let targetElement = document.querySelector(targetId);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
